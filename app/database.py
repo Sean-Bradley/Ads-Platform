aa50e0ea59202ea4d52f9bd38718bb1c353723a8
root <root@Johan.localdomain>
2026-08-26 22:43:26 +0200

    Add environment-based application conf

--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.config import DATABASE_URL
 
-DATABASE_URL = "sqlite:///./ads.db"
 
 engine = create_engine(
     DATABASE_URL,
